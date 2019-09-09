8b1d0aefe2e33b99cc3ea4aa4c116977d6d94bec
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-09-09 15:59:58 -0400

    working maze

---
+++ public/lib/Cell.js
@@ -0,0 +1,96 @@
+function Cell(i, j) {
+	this.i = i
+	this.j = j
+	this.walls = [true, true, true, true]
+	this.visited = false
+}
+
+Cell.prototype.randomizeWalls = function () {
+	const { waslls } = this
+	for (let i = 0; i < walls.length; i++) {
+		this.walls[i] = (generateRandomNumber() < 5)
+	}
+}
+
+Cell.prototype.removeWall = function(current) {
+	const { i, j, walls } = this
+	const diffI = i - current.i
+	const diffJ = j - current.j
+	if (diffI > 0 && diffJ == 0) {
+		current.walls[1] = false
+		walls[3] = false
+	}
+	if (diffI < 0 && diffJ == 0) {
+		current.walls[3] = false
+		walls[1] = false
+	}
+
+	if (diffJ < 0 && diffI == 0) {
+		current.walls[0] = false
+		walls[2] = false
+	}
+	if (diffJ > 0 && diffI == 0) {
+		current.walls[2] = false
+		walls[0] = false
+	}
+}
+
+Cell.prototype.show = function () {
+	const { i, j, walls, visited } = this
+	const x = i * w
+	const y = j * h
+	const [top, right, bottom, left] = walls
+	stroke(0)
+	if (top) line(x		, y		 , x + w, y		 ) //top
+	if (right) line(x + w, y		 , x + w, y + h) //right
+	if (bottom) line(x    , y + h, x + w, y + h) //bottom
+	if (left) line(x		, y		 , x		, y + h) //left
+	// line(x, y, x+w,y)
+	// line(x+w,y,x+w,y+h)
+	// line(x+w,y+w,x,y+w)
+	// line(x, y+w,x, y)
+	if (visited) {
+		noStroke()
+		fill(255)
+		rect(x, y, w, h)
+	}
+}
+
+Cell.prototype.checkNeighbors = function(grid) {
+	const { i, j } = this
+	let validPoint = function([x, y], arr) {
+		if (x < 0 || x > arr.length - 1) {
+			return false
+		}
+
+		if (y < 0 || y > arr[0].length - 1) {
+			return false
+		}
+		return arr[x][y]
+	}
+
+
+	const neighbors = [
+		validPoint([i    , j - 1], grid),
+		validPoint([i + 1, j    ], grid),
+		validPoint([i    , j + 1], grid),
+		validPoint([i - 1, j    ], grid)
+	]
+	const notVisitedNeighbors = neighbors.filter(x => x && !x.visited)
+	const randomNeighbor = notVisitedNeighbors.length > 0 && notVisitedNeighbors[Math.floor(Math.random() * notVisitedNeighbors.length)]
+	return randomNeighbor
+}
+
+Cell.prototype.highlight = function () {
+	const { i, j } = this
+	const x = i * w
+	const y = j * h
+	noStroke()
+	fill(0, 255, 0)
+	rect(x, y, w, h)
+}
+
+
+function generateRandomNumber() {
+	return Math.floor(Math.random() * 10) + 1
+}
--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,65 @@
+let cols = 25
+let rows = 25
+let w, h
+let grid, cellGrid
+let current
+
+function generateCellGrid(grid) {
+	let cellGrid = Object.assign([], grid)
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			cellGrid[i][j] = new Cell(i, j)
+			// cellGrid[i][j].randomizeWalls()
+		}
+	}
+	return cellGrid
+}
+
+function generateGrid(x, y) {
+	let grid = new Array(x)
+	for (let i = 0; i < cols; i++) {
+		grid[i] = new Array(y)
+	}
+	return grid
+}
+
+
 function setup() {
-	let canvas = createCanvas(window.screen.width-200, window.screen.height-200)
+	let canvas = createCanvas(window.screen.width, window.screen.height)
 	canvas.parent('root')
+	frameRate(5)
+
+	w = Math.floor(width / rows)
+	h = Math.floor(height / cols)
+
+	grid = generateGrid(rows, cols)
+	cellGrid = generateCellGrid(grid)
+
+	current = cellGrid[0][0]
+	current.visited = true
 }
 
 function draw() {
 	// background(0)
+	for (let i = 0; i < cellGrid.length; i++) {
+		for (let j = 0; j < cellGrid[i].length; j++) {
+			cellGrid[i][j].show()
+		}
+	}
+	current.highlight()
+	const neighbor = current.checkNeighbors(cellGrid)
+	if (neighbor) {
+		//step 2
+		//
+		
+		
+		neighbor.removeWall(current)
+		neighbor.visited = true
+		current = neighbor
+	}
+
 }
+
+
+
+
